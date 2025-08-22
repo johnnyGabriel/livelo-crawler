e451842da4b344b136b0acd0f432403591849ef0
Johnny Pestana <johnnygpestana@gmail.com>
2025-08-22 18:49:38 -0300

    adding Redis and Flask to project

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from flask import Flask
 from livelocrawler.crawler import LiveloCrawler
+from redis import Redis
 
+api = Flask(__name__)
+redis = Redis(host='localhost', port=6379, decode_responses=True)
 crawler = LiveloCrawler()
 
+# getting data from crawler
 partners = crawler.get_partners_data()
+partners_dict = dict(zip(partners['Partner'], partners['Score']))
 
-print(f'{partners=}')
+# redis saving
+redis.hset('partners', mapping=partners_dict)
+redis.expire('partners', 100)
+
+# api handler
+@api.route('/')
+def hello_world():
+    return '<p>Hello World</p>'
+
+
