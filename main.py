f7de77c8ee791312b0786155ee39e9c10407bb76
Johnny Pestana <johnnygpestana@gmail.com>
2025-08-23 23:58:03 -0300

    setting api response

--- main.py
+++ main.py
@@ -2,21 +2,36 @@ from flask import Flask
 from livelocrawler.crawler import LiveloCrawler
 from redis import Redis
 
+REDIS_HASH = 'partners'
+REDIS_CACHE_EXPIRE_5H = 18000
+
 api = Flask(__name__)
 redis = Redis(host='localhost', port=6379, decode_responses=True)
 crawler = LiveloCrawler()
 
-# getting data from crawler
-partners = crawler.get_partners_data()
-partners_dict = dict(zip(partners['Partner'], partners['Score']))
+def get_partners():
+    partners = redis.hgetall(REDIS_HASH)
+    if partners == {}:
+        partners = crawler.get_partners_data()
+        partners = dict(zip(partners['Partner'], partners['Score']))
+        redis.hset(REDIS_HASH, mapping=partners)
+        redis.expire(REDIS_HASH, REDIS_CACHE_EXPIRE_5H)
+    return partners
 
-# redis saving
-redis.hset('partners', mapping=partners_dict)
-redis.expire('partners', 100)
+def transform_to_response(partners: dict):
+    res = []
+    for partner_name, score in partners.items():
+        res.append({
+            'partner_name': partner_name,
+            'score': score
+        })
+    return res
 
 # api handler
+# run: flask --app main run
 @api.route('/')
-def hello_world():
-    return '<p>Hello World</p>'
+def get_all_partners():
+    partners = get_partners()
+    return transform_to_response(partners)
 
 
